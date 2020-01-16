fe1cf2f9281b74e6609eff47bba6d32dd43b19b0
bpharris <brandon.harris1@btinternet.com>
2020-01-16 01:43:43 +0000

    The great refactor of 2020

--- headfrick.py
+++ headfrick.py
@@ -4,93 +4,106 @@ from argparse import ArgumentParser, FileType
 
 
 class Memory(list):
-    """Class representing a Programs memory, subclass of list."""
+    """
+    Class representing a Machine's memory, subclass of list. Memory is lazy
+    and arbitrary length.
+    """
+
     def stretch(self, index):
         """Stretch the memory to contain the index, populating empty cells."""
         if index >= len(self):
             self += [0] * (index - len(self) + 1)
 
-    def __getitem__(self, index):
-        self.stretch(index)  # On read from cell, stretch memory if needed
+    def __getitem__(self, index: int) -> int:
+        # On read from cell, stretch memory if needed
+        self.stretch(index)
+
         return super().__getitem__(index)
-    
-    def __setitem__(self, index, value):
-        self.stretch(index)  # On write to cell, stretch memory if needed
+
+    def __setitem__(self, index: int, value: int) -> int:
+        # On write to cell, stretch memory if needed
+        self.stretch(index)
+
         return super().__setitem__(index, value)
-    
+
     def __repr__(self) -> str:
         return super().__repr__()[:-1] + ', ... ]'
 
 
 class Machine:
-    """Class representing brainfuck virtual machine."""
+    """Class representing brainfuck/headfrick virtual machine."""
     CELL_MIN = 0        # Minimum cell value
     CELL_MAX = 255      # Maximum cell value
 
     INSTRUCTION_SET = set(['>', '<', '+', '-', '.', ',', '[', ']'])
 
-    # TODO Fix issue where cell or 1 can't be decremented
-    overflow  = lambda v: v if v <= Machine.CELL_MAX else Machine.CELL_MIN
-    underflow = lambda v: v if v >= Machine.CELL_MIN else Machine.CELL_MAX
-
     def __init__(self) -> None:
-        """Create pointer and memory of running program."""
+        """Create pointer and memory."""
         self.pointer = 0
         self.memory = Memory([0])
 
-    def run_program(self, program) -> None:
+    def run_program(self, program: str) -> None:
         """Run the given brainfuck program on this machine."""
-        for instruction in program:
-            self.run_instruction(instruction)
+        instruction_pointer = 0
 
-    def run_instruction(self, instruction) -> None:
-        """Run the given instruction on the machine."""
-        if instruction == 'p':
-            return
+        while instruction_pointer < len(program):
+            instruction = program[instruction_pointer]
 
-        if instruction == '>':
-            self.pointer += 1
-        if instruction == '<':
-            self.pointer = max(0, self.pointer - 1)
+            if instruction == '>':
+                self.pointer += 1
+            if instruction == '<':
+                self.pointer = max(0, self.pointer - 1)
 
-        if instruction == '+':
-            self.current(self.current() + 1)
-        if instruction == '-':
-            self.current(self.current() - 1)
+            if instruction == '+':
+                self.set_current(self.memory[self.pointer] + 1)
+            if instruction == '-':
+                self.set_current(self.memory[self.pointer] - 1)
 
-        if instruction == '.':
-            print(chr(self.current()))
-        if instruction == ',':
-            self.current(ord(get_char()))
+            if instruction == '.':
+                print(chr(self.current()))
+            if instruction == ',':
+                self.set_current(ord(get_char()))
+            
+            if instruction == '[':
+                # Read to closing-]
+                stack = list('[')
+                internal_instructions = str()
+                closing_instruction = instruction_pointer
 
-        if instruction == '[':
-            return
-        if instruction == ']':
-            return
-    
-    def __jump_forwards(self):
-        """Brainfuck jump forwards instruction."""
-        pass
+                while stack:
+                    instruction_pointer += 1
 
-    def __jump_backwards(self):
-        """Brainfuck jump backwards instruction."""
-        pass
+                    if program[instruction_pointer] == '[':
+                        stack.append('[')
+                    if program[instruction_pointer] == ']':
+                        stack.pop()
+                        closing_instruction = instruction_pointer
+                    
+                    internal_instructions += program[instruction_pointer]
+                
+                # Remove closing-]
+                internal_instructions = internal_instructions[:-1]
 
-    def current(self, value=None):
-        """Return the value at the pointer."""
-        if value:
-            return self.__set_current(value)
-        return self.memory[self.pointer]
-    
-    def __set_current(self, value):
-        """Set the value at the pointer."""
-        self.memory[self.pointer] = Machine.overflow(Machine.underflow(value))
+                while self.memory[self.pointer] != Machine.CELL_MIN:
+                    self.run_program(internal_instructions)
+
+                instruction_pointer = closing_instruction
+            if instruction == ']':
+                raise SyntaxError("Invalid instruction '{}'.".format(instruction))
+
+            # Increment instruction pointer
+            instruction_pointer += 1
+
+    def set_current(self, value: int) -> None:
+        """Set the value at the current pointer location."""
+        self.memory[self.pointer] = value % (Machine.CELL_MAX + 1)
 
     def __repr__(self) -> str:
         """Return string representation."""
         # On print, stretch memory to show pointer movement
         self.memory.stretch(self.pointer)
 
+        # TODO: Fix tracking when numbers > 1 digit
         return str(self.memory) + '\n ' + self.pointer * '   ' + '^'
 
 
@@ -147,7 +160,7 @@ def main() -> None:
 
     if args.dump:
         print(machine)
-    
+
     return None
 
 
